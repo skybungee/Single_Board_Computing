9df1c4dc3d8808206fd65fb01e8612ef7ed9d58e
mvbingham <mv.bingham@gmail.com>
2021-06-11 13:09:52 -0400

    added line to run stats

--- raspberry_pi/2-install_oled_files.sh
+++ raspberry_pi/2-install_oled_files.sh
@@ -21,7 +21,7 @@ sudo i2cdetect -y 1
 sudo python3 -m pip install --upgrade pip setuptools wheel
 git clone https://github.com/adafruit/Adafruit_Python_SSD1306.git
 cd Adafruit_Python_SSD1306/
-sudo python3 setup.py install
+sudo ~/Single_Board_Computing/raspberry_pi/Adafruit_Python_SSD1306/examplespython3 setup.py install
 python3 stats.py
 
 ###############################################################################################
