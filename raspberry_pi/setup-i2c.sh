360965ea4f8eb869494cbfb18153968edc3fe15a
mvbingham <mv.bingham@gmail.com>
2021-06-11 13:05:14 -0400

    removed lines from i2c script

--- raspberry_pi/setup-i2c.sh
+++ raspberry_pi/setup-i2c.sh
@@ -12,16 +12,6 @@ fi
  
 # enable I2C on Raspberry Pi
 echo '>>> Enable I2C'
-if grep -q 'i2c-bcm2708' /etc/modules; then
-  echo 'Seems i2c-bcm2708 module already exists, skip this step.'
-else
-  echo 'i2c-bcm2708' >> /etc/modules
-fi
-if grep -q 'i2c-dev' /etc/modules; then
-  echo 'Seems i2c-dev module already exists, skip this step.'
-else
-  echo 'i2c-dev' >> /etc/modules
-fi
 if grep -q 'dtparam=i2c1=on' /boot/config.txt; then
   echo 'Seems i2c1 parameter already set, skip this step.'
 else
@@ -32,13 +22,7 @@ if grep -q 'dtparam=i2c_arm=on' /boot/config.txt; then
 else
   echo 'dtparam=i2c_arm=on' >> /boot/config.txt
 fi
-if [ -f /etc/modprobe.d/raspi-blacklist.conf ]; then
-  sed -i 's/^blacklist spi-bcm2708/#blacklist spi-bcm2708/' /etc/modprobe.d/raspi-blacklist.conf
-  sed -i 's/^blacklist i2c-bcm2708/#blacklist i2c-bcm2708/' /etc/modprobe.d/raspi-blacklist.conf
-else
-  echo 'File raspi-blacklist.conf does not exist, skip this step.'
-fi
- 
+
 # install i2c-tools
 echo '>>> Install i2c-tools'
 if hash i2cget 2>/dev/null; then
