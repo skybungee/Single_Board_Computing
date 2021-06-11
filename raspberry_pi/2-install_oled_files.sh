4629ebfad133d9a5e472a1db7164bdc286ad26d7
mvbingham <mv.bingham@gmail.com>
2021-06-11 12:22:14 -0400

    added -y flag to end of apt-get

--- raspberry_pi/2-install_oled_files.sh
+++ raspberry_pi/2-install_oled_files.sh
@@ -10,16 +10,15 @@ set -e
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
 #
 ##################################################################################################################
-sudo apt-get install python-smbus
-sudo apt-get install i2c-tools
-sudo apt-get install python3-pip
+sudo apt-get install python-smbus -y
+sudo apt-get install i2c-tools -y
+sudo apt-get install python3-pip -y
 sudo pip3 install Adafruit_BBIO
 sudo pip3 install image
-sudo apt-get install libopenjp2-7
-sudo apt install libtiff5
+sudo apt-get install libopenjp2-7 -y
+sudo apt install libtiff5 -y
 sudo i2cdetect -y 1
 sudo python3 -m pip install --upgrade pip setuptools wheel
-sudo apt-get install git
 git clone https://github.com/adafruit/Adafruit_Python_SSD1306.git
 cd Adafruit_Python_SSD1306/
 sudo python3 setup.py install
