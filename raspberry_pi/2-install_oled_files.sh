aa68f4f7944158ab1b933769adeb7cf4f50c557f
mvbingham <mv.bingham@gmail.com>
2021-06-11 13:16:30 -0400

    added line to setup python

--- raspberry_pi/2-install_oled_files.sh
+++ raspberry_pi/2-install_oled_files.sh
@@ -21,8 +21,8 @@ sudo i2cdetect -y 1
 sudo python3 -m pip install --upgrade pip setuptools wheel
 git clone https://github.com/adafruit/Adafruit_Python_SSD1306.git
 cd Adafruit_Python_SSD1306/
-sudo ~/Single_Board_Computing/raspberry_pi/Adafruit_Python_SSD1306/examplespython3 setup.py install
-python3 stats.py
+sudo python3 setup.py install
+sudo python3 ~/Single_Board_Computing/raspberry_pi/Adafruit_Python_SSD1306/examples/stats.py
 
 ###############################################################################################
 echo "################################################################"
